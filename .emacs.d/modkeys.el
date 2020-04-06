20b2eb48c8f25ac3beab95c30be3ae88b2ea7ae8
Sourena Yadegari <srn.yad@gmail.com>
2020-04-06 21:09:45 +0200

    f2 is now shortkey for helm buffers. Ibuffer is mapped to C-c C-b.

--- .emacs.d/modkeys.el
+++ .emacs.d/modkeys.el
@@ -1,10 +1,16 @@
 ;; modified keys
+(defun nw-kbd (binding command)
+  "If the command has keybidings that cannot be when in terminal use this"
+  (when (eq window-system nil)
+    (global-set-key (kbd binding) command)))
+
 (defun modify-keybinds () (progn
 			    (global-set-key (kbd "<f9>") 'kill-this-buffer)  ; kill buffer
 			    (global-set-key (kbd "<f5>") 'other-window)      ; other window!
 			    (global-set-key (kbd "<f6>") (kbd "C-- C-x o") ) ; other window backward
-			    (global-set-key (kbd "<f2>") (kbd "C-x C-b") )   ; ibuffer
-			    (global-set-key (kbd "<f1>") 'helm-M-x )   ; ibuffer
+			    (global-set-key (kbd "<f2>") 'helm-buffers-list) ; helm buffer list 
+			    (global-set-key (kbd "C-x C-b") 'ibuffer)        ; ibuffer  
+			    (global-set-key (kbd "<f1>") 'helm-M-x )         ; helm
 			    (global-set-key (kbd "<f4>") 'shell)             ; opens a shell
 			    (global-set-key (kbd "<f7>") 'hs-toggle-hiding)
 			    (global-set-key (kbd "<f10>") 'delete-window)
